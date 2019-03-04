140710f83f89948bd52b40de41b5e27499eb1c0d
firnandakusuma <39391888+firnandakusuma@users.noreply.github.com>
2019-03-04 22:41:43 +0700

    pengumpulan JS 3 dan Gitbook

--- Praktikum/Gitbook/SUMMARY.md
+++ Praktikum/Gitbook/SUMMARY.md
@@ -2,3 +2,4 @@
 
 * [Introduction](README.md)
 * [2. Typescript](2.Typescript.md)
+* [3. Angular Fundamental](3.AngularFundamental.md)
---
+++ Praktikum/Joobsheet/3. AngularFundamental/Firnanda-TI2B/ts-hello/main.ts
@@ -0,0 +1,15 @@
+class Pointku{
+    a:number;
+    b:number;
+
+    draw(){
+        console.log('X' + this.a + ',Y'+this.b);
+    }
+
+    getDistance(another:Pointku1){
+        //....
+    }
+}
+
+let coins:Pointku1
+coins.draw();
