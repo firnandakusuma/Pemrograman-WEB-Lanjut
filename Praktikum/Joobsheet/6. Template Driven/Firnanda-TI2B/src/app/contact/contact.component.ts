073ef05799bde6a73e177e08c70c560ddf6af170
firnandakusuma <39391888+firnandakusuma@users.noreply.github.com>
2019-04-24 22:27:19 +0700

    Pengumpulan JS 6

--- Praktikum/Joobsheet/6. Template Driven/Firnanda-TI2B/src/app/contact/contact.component.ts
+++ Praktikum/Joobsheet/6. Template Driven/Firnanda-TI2B/src/app/contact/contact.component.ts
@@ -16,6 +16,12 @@ import { Component, OnInit, Input } from '@angular/core';
 
 // }
 export class ContactComponent {
+
+  contactMethods=[
+    {id:1, name:'email'},
+    {id:2, name:'phone'}
+  ]
+
   log(z) {
     console.log(z);
   }
