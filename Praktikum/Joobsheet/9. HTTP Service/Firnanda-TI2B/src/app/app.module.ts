a0354712517ba74dbf9720057eef2702d180d4d9
firnandakusuma <39391888+firnandakusuma@users.noreply.github.com>
2019-04-24 15:44:16 +0700

    pengumpulan JS 9

--- Praktikum/Joobsheet/9. HTTP Service/Firnanda-TI2B/src/app/app.module.ts
+++ Praktikum/Joobsheet/9. HTTP Service/Firnanda-TI2B/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
